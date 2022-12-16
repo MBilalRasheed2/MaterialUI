4af769dbd0090f5746447c9174b5130605e4718d
git-react41 <git-react41@mathieuholding.com>
2022-12-17 00:22:36 +0500

    Checkout stepper

---
+++ src/components/checkout/CheckoutStepper.js
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepButton from '@mui/material/StepButton';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ShoppingAddress from './ShoppingAddress';
+import PaymentMethod from './PaymentMethod';
+import OrderSummary from './OrderSummary';
+import Thankyou from './Thankyou';
+import { Paper } from '@mui/material';
+
+const steps = ['Shipping address', 'Payment details', 'Review your order'];
+
+export default function HorizontalNonLinearStepper() {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState({});
+
+    const totalSteps = () => {
+        return steps.length;
+    };
+    console.log(completed)
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
+    };
+
+    const handleNext = () => {
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                // find the first step that has been completed
+                steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStep = (step) => () => {
+        setActiveStep(step);
+    };
+
+    const handleComplete = () => {
+        const newCompleted = completed;
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        handleNext();
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+        setCompleted({});
+    };
+
+    return (
+        <Paper sx={{ width: '100%', marginY: '20px',py:4 }} elevation={2}>
+            <Stepper nonLinear activeStep={activeStep}>
+                {steps.map((label, index) => (
+                    <Step key={label} completed={completed[index]}>
+                        <StepButton color="inherit" onClick={handleStep(index)}>
+                            {label}
+                        </StepButton>
+                    </Step>
+                ))}
+            </Stepper>
+            <div>
+                {allStepsCompleted() ? (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleReset}>Reset</Button>
+                        </Box>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <LoadStepForm step={activeStep} />
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Button
+                                color="inherit"
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                sx={{ mr: 1 }}
+                            >
+                                Back
+                            </Button>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleNext} sx={{ mr: 1 }}>
+                                Next
+                            </Button>
+                            {activeStep !== steps.length &&
+                                (completed[activeStep] ? (
+                                    <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                        Step {activeStep + 1} already completed
+                                    </Typography>
+                                ) : (
+                                    <Button onClick={handleComplete}>
+                                        {completedSteps() === totalSteps() - 1
+                                            ? 'Finish'
+                                            : 'Complete Step'}
+                                    </Button>
+                                ))}
+                        </Box>
+                    </React.Fragment>
+                )}
+            </div>
+        </Paper>
+    );
+}
+const LoadStepForm = ({ step }) => {
+    if (step === 0) {
+        return <ShoppingAddress />
+    }
+    else if (step === 1) {
+        return <PaymentMethod />
+    }else if(step === 2) {
+        return <OrderSummary/>
+    }else{
+        return <Thankyou/>
+    }
+}
